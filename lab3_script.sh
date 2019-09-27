d587f46dbe3f51b074272d0485c4f061493445d5
ColoraodRequiem <thma4535@colorado.edu>
2019-09-26 19:11:59 -0600

    Adding email_results.txt

--- lab3_script.sh
+++ lab3_script.sh
@@ -11,7 +11,8 @@ echo "Input a regular expression: "
 read exp # get input
 grep $exp $filename
 grep @geocities.com regex_practice.txt >> email_results.txt
-grep -c -p 303-[0-9]{3}-[0-9]{4} regex_practice.txt
+grep -c -P 303-[0-9]{3}-[0-9]{4} regex_practice.txt
+
 git init
 git add .
 git commit -m "Adding email_results.txt"
